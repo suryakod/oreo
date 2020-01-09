f020e4a8c36bd4921ea1ee49b4655a5b0cd60de1
vcharan98 <36467316+vcharan98@users.noreply.github.com>
2020-01-09 17:24:17 +0100

    deleting unnecessary files

--- Userclass.py
+++ Userclass.py
@@ -46,7 +46,37 @@ class User:
 
     
     def login(self,userId,psw):
+
+        login = False
+        logindata = pandas.read_csv('root/login.csv')
+        loginuser = pandas.read.csv('root/users.csv')
         
+        tmoment = pandas.DataFrame(columns=['username'])
+        tmoment['username'] = [user]
+        loginuser = loginuser.append(tmoment)
+        loginuser.to_csv('root/users.csv', index=False)
+
+
+        if login:
+            print("\nAlready logged in")            
+        if user not in logindata['username'].tolist():
+            print("\nUsername not registered")
+        if password != logindata.loc[logindata['username'] == user, 'password'].iloc[0]:
+            print("\nWrong password!")
+        if user in loginuser['username'].tolist():
+            print("\nLogged in from different address")
+        print("\nLogin completed.")    
+
+        try:
+            if user in loginuser['username'].tolist():
+                usrlist = loginuser['username'].tolist().remove(user)
+                loginuser['username'] = usrlist
+                loginuser.to_csv("root/users.csv", index=False)
+            user = None
+            rdindex = {}
+            print("\nSigned out")
+        except KeyError:
+            print("\nSigned out")
 
     def delete(self,userId,pws):
         pass
