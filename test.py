ea5cccc62b57bc190fd831fd10ec40e2c373c30d
suryakod <54937719+suryakod@users.noreply.github.com>
2020-01-25 16:58:10 +0100

    tests

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas
 import sys
-import Userclass
+from Userclass import User
 
 class clienttest(unittest.TestCase):
 
@@ -14,8 +14,9 @@ class clienttest(unittest.TestCase):
         tlog['password'] = ['1234']
         tlog['isAdmin'] = 1
 
-        Userclass.createdusers = tlog
-        expresults = ["\nWrong password!", "\nUsername not registered", "\nLogin completed."]
+        user_test = User()
+        user_test.loggedinusers = tlog
+        expresults = ['\nWrong password!', '\nUsername not registered', '\nLogin completed.']
         obtresults = []
         tests = [
             ['test', '1234'],
@@ -24,19 +25,48 @@ class clienttest(unittest.TestCase):
         ]
 
         for test in tests:
-            obtresults.append(Userclass.login(
+            obtresults.append(user_test.login(
                 test[0], test[1]))
-        Userclass.quit()
+        user_test.quit()
+        login_rst = pandas.DataFrame(columns = ['username'])
+        login_rst.to_csv('ServerAccessSession/Users.csv', index = False)
 
         self.assertListEqual(obtresults, expresults)
+
+
+    def test_registration(self):
+        tlog = pandas.DataFrame(columns=['username'])
+        tlog['username'] = ['test']
+        tlog['password'] = ['1234']
+        tlog['isAdmin'] = 1
+
+        user_test = User()
+        user_test.createdusers = tlog
+        expresults = ['\nUsername not available']
+        obtresults = []
+        tests = [
+            ['test', '1234', 'Admin'],
+            ['test2', '123', 'Admin'],
+            ['test', '123', 'Admin']
+        ]
+
+        for test in tests:
+            obtresults.append(user_test.register(
+                test[0], test[1], test[2]))
+        user_test.quit()
+        login_rst = pandas.DataFrame(columns = ['username'])
+        login_rst.to_csv('ServerAccessSession/Users.csv', index = False)
+
+        self.assertListEqual(obtresults, expresults)
+
 
 def step_completed(test_to_use):
 
     load = unittest.TestLoader()
     suite = unittest.TestSuite()
 
-    suite.addTests(loader.loadTestsFromTestCase(test_to_use))
-    runtest = unittest.TextTestResult(verbosity=2)
+    suite.addTests(load.loadTestsFromTestCase(test_to_use))
+    runtest = unittest.TextTestRunner(verbosity = 2)
     result = runtest.run(suite)
 
     if result.skipped:
