cc9efdc708e3a9be5918b10f1eff23372b9c9149
suryakod <54937719+suryakod@users.noreply.github.com>
2020-01-26 16:31:12 +0100

    changes

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import sys
 import pandas
 from Userclass import User
 
-class client_test(unittest.TestCase):
+class clientTest(unittest.TestCase):
     '''
     This class defines the tests that are conducted
     for the functions defined in the client-server application
@@ -131,7 +131,7 @@ def testing():
     This function executes the function of step_completed
     '''
     print('*'*60 + "\nTesting:\n")
-    return step_completed(client_test)
+    return step_completed(clientTest)
 
 if __name__ == "__main__":
     if testing() is not True:
