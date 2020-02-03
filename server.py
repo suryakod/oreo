8b2d88065a93898d632b0aed95fd392d1e953c64
suryakod <54937719+suryakod@users.noreply.github.com>
2020-02-03 20:11:15 +0100

    chaanges

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ def client_request(usr, message):
         return "Enter correct command"
     if message.split(" ")[0] == "write_file":
         if len(message.split(" ")) >= 2:
-            return usr.write_file(message.split(" ")[1]," ".join(message.split(" ")[2:]))
+            return usr.write_file(message.split(" ")[1], " ".join(message.split(" ")[2:]))
         return "Enter correct command"
     if message.split(" ")[0] == "create_folder":
         if len(message.split(" ")) == 2:
